b7d00eb72a2821090ff5d3ad3c6cdbd815fe3199
Jack <62153707+JackCo100@users.noreply.github.com>
2025-07-06 16:02:26 +0100

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -18,67 +18,63 @@ function renderOpenDay(data: any) {
   }
   
   app.innerHTML = `
-    
-<div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
-        <a href="https://www.cardiff.ac.uk/" target="_blank" rel="noopener noreferrer">
-          <img src="${cuLogo}" alt="Cardiff University Logo" class="h-16 w-auto" />
-        </a>
-      </div>
-      <h1 class="text-3xl sm:text-5xl font-bold text-cardiff-red mb-2 text-center">Cardiff University Open Day</h1>
-      <p class=" text-l text-cardiff-dark mb-4">Welcome to the Cardiff University Open Day website, here you can find out about all the events by school across campus.</p>
-      <div class="grid gap-4 grid-cols-1 sm:grid-cols-1 lg:grid-cols-1">
-        ${data.topics.map((topic: any) => topic && topic.name ? `
-          <div class="w-100 p-2">
-            <div class="border border-gray-400 p-2">
-              <div class="mb-4">
-                <img class="h-30 w-30 object-cover" src="${topic.cover_image}" />
-                <div class="text-cardiff-red font-bold text-xl mb-2">${topic.name}</div>
-                <p class="text-cardiff-dark text-base">${topic.description || ''}</p>
-              </div>
-              
-                ${topic.programs && topic.programs.length ? `
-              <h2 class="text-cardiff-red font-bold mb-2"> Event Calendar </h2>
-              <table class="table-fixed border-collapse border border-cardiff-light-grey w-full">
-                <thead>
-                  <tr>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-4/10">Event Name</th>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Start Time</th>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">End Time</th>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-2/10">Building Address</th>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Room</th>
-                    <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Info</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  ${topic.programs.map((program: any) => program && program.title ? `
-                    <tr>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.title}</td>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${new Date(program.start_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</td>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${new Date(program.end_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</td>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.location.title}, ${program.location.address}</td>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.room ? program.room : 'Reception'}</td>
-                      <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey"> <button class="btn bg-cardiff-red text-white" onClick="mod${program.id}.showModal()"')">More Info</button> </td>
-                    </tr>
-                    <dialog id="mod${program.id}">
-                      <div class="bg-white p-4">
-                        <h2 class="m-4 text-cardiff-red">${program.title}</h2>
-                        <p class="text-cardiff-dark">${program.description}</p>
-                        <button onClick="mod${program.id}.close()" class="mt-4">Close</button>
-                        </div>
-                      </dialog>
-                  `: '').join('')}
-                </ul>
-              </div>
-            ` : 'No events scheduled!'}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          ` : '').join('')}
-      </div>
+    <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
+      <a href="https://www.cardiff.ac.uk/" target="_blank" rel="noopener noreferrer">
+        <img src="${cuLogo}" alt="Cardiff University Logo" class="h-16 w-auto" />
+      </a>
     </div>
-     
+    <h1 class="text-3xl sm:text-5xl font-bold text-cardiff-red mb-2 text-center">Cardiff University Open Day</h1>
+    <p class=" text-l text-cardiff-dark mb-4">Welcome to the Cardiff University Open Day website, here you can find out about all the events by school across campus.</p>
+    <div class="grid gap-4 grid-cols-1 sm:grid-cols-1 lg:grid-cols-1">
+      ${data.topics.map((topic: any) => topic && topic.name ? `
+        <div class="w-100 p-2">
+          <div class="border border-gray-400 p-2">
+            <div class="mb-4">
+              <img class="h-30 w-30 object-cover" src="${topic.cover_image}" />
+              <div class="text-cardiff-red font-bold text-xl mb-2">${topic.name}</div>
+              <p class="text-cardiff-dark text-base">${topic.description || ''}</p>
+            </div>
+              ${topic.programs && topic.programs.length ? `
+            <h2 class="text-cardiff-red font-bold mb-2"> Event Calendar </h2>
+            <table class="table-fixed border-collapse border border-cardiff-light-grey w-full">
+              <thead>
+                <tr>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-4/10">Event Name</th>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Start Time</th>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">End Time</th>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-2/10">Building Address</th>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Room</th>
+                  <th class="text-cardiff-red font-bold border-b border-cardiff-light-grey bg-gray-200 w-1/10">Info</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${topic.programs.map((program: any) => program && program.title ? `
+                  <tr>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.title}</td>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${new Date(program.start_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</td>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${new Date(program.end_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</td>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.location.title}, ${program.location.address}</td>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey">${program.room ? program.room : 'Reception'}</td>
+                    <td class="text-cardiff-dark text-wrap border-b border-cardiff-light-grey"> <button class="btn bg-cardiff-red text-white" onClick="mod${program.id}.showModal()"')">More Info</button> </td>
+                  </tr>
+                  <dialog id="mod${program.id}">
+                    <div class="bg-white p-4">
+                      <h2 class="m-4 text-cardiff-red">${program.title}</h2>
+                      <p class="text-cardiff-dark">${program.description}</p>
+                      <button onClick="mod${program.id}.close()" class="mt-4">Close</button>
+                      </div>
+                    </dialog>
+                `: '').join('')}
+              </ul>
+            </div>
+          ` : 'No events scheduled!'}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        ` : '').join('')}
+    </div>
+  </div>
   `
 }
-
 loadOpenDay().then(renderOpenDay)
