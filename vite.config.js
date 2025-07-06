80e87ee60607104f2b649517569650f7c7336f09
Jack <62153707+JackCo100@users.noreply.github.com>
2025-07-06 17:04:39 +0100

    repo fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/CUOpenDayTest/' // Change to your repo name
+  base: 'https://jackco100.github.io/CUOpenDayTest/' // Change to your repo name
 })
